cc627a4a48894b040bb0860657633adf66647e16
Luís Roque <luis_roque@live.com>
2025-02-17 20:52:06 +0000

    bugfix best hyperparams in running script

--- hitgen/experiments/testing_HiTGen.py
+++ hitgen/experiments/testing_HiTGen.py
@@ -322,8 +322,8 @@ if __name__ == "__main__":
                 original_dyn_features,
                 window_size=WINDOW_SIZE,
                 stride=create_dataset_vae.stride_temporalize,
-                batch_size=BATCH_SIZE_HITGEN,
-                shuffle=SHUFFLE_HITGEN,
+                batch_size=create_dataset_vae.best_params["batch_size"],
+                shuffle=create_dataset_vae.best_params["shuffle"],
             )
 
             _, synth_hitgen_test_long, _, _, synth_hitgen_test_long_no_transf, _ = (
@@ -332,7 +332,7 @@ if __name__ == "__main__":
                     data_mask_temporalized=data_mask_temporalized,
                     samples=data_mask_temporalized.indices.shape[0],
                     window_size=WINDOW_SIZE,
-                    latent_dim=LATENT_DIM_HITGEN,
+                    latent_dim=create_dataset_vae.best_params["latent_dim"],
                 )
             )
 
